2d9ccd4b8f855007666e1070598eaad118bd1ba1
Mickael <mickaelluis100@gmail.com>
2024-04-12 23:00:19 -0300

    banner-002

---
+++ JS-script/Banner.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.js-baner-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+  });
